dd942188b690d023b29331990a6acc0d190f38ed
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-06-13 20:42:19 +0200

    Update diesel to `2.1`

--- src/infrastructure/schema.rs
+++ src/infrastructure/schema.rs
@@ -3,8 +3,10 @@
 diesel::table! {
     adverts (id) {
         id -> Int4,
+        #[max_length = 100]
         title -> Varchar,
         description -> Text,
+        #[max_length = 200]
         photo -> Varchar,
         price -> Money,
         created_at -> Timestamptz,
