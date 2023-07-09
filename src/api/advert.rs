5913849b5a5b1901f463ef77ddf736018e8f2132
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-07-09 18:57:43 +0200

    Add route to delete API

--- src/api/advert.rs
+++ src/api/advert.rs
@@ -5,5 +5,6 @@ pub fn create_service(service: &mut web::ServiceConfig) {
     service
         .route("", web::get().to(advert::list))
         .route("/{id}", web::get().to(advert::get))
+        .route("/{id}", web::delete().to(advert::delete))
         .route("", web::post().to(advert::create));
 }
